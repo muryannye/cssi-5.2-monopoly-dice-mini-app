cc9507634abd2fb83612d27dc3a7e1197305e869
gEvent <gevent@CSSI2015-11.local>
2015-07-17 12:55:04 -0400

    Basic dice roller

--- monopoly.py
+++ monopoly.py
@@ -2,3 +2,10 @@ import random #imports random library.
 
 #Write your monopoly dice roller app here!
 #You can make use of random.randint()
+dice1 = random.randint(1,6)
+dice2 = random.randint(1,6)
+total = dice1 + dice2
+if dice1 == dice2:
+    print "Doubles! Move %d spaces and roll again." % (total)
+else:
+    print "Move %d spaces. Next player's turn!" % (total)
